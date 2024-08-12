132654beaa1594fc85d53e8d2db7b6dcbf247d8b
Alex Auch <alexsamuelauch@gmail.com>
2024-08-12 09:31:48 -0600

    added build script and adjusted server to serve frontend files

--- urban-greens-backend/src/server.js
+++ urban-greens-backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import productRoutes from './routes/products.js';
 import blogRoutes from './routes/blogs.js';
@@ -8,11 +10,19 @@ import contactRoutes from './routes/contacts.js';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 // Middleware
 app.use(express.json());
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, '../../urban-greens-frontend/build')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../urban-greens-frontend/build', 'index.html'));
+});
 // Database connection
 mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB connected'))
